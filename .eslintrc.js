1675c07814fe22ddc80a088da7b7845b915b1565
Unknown <s1996ch@gmail.com>
2018-07-30 09:35:55 +0430

    added element-ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,4 +47,4 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import {
+  Row,
+  Button,
+  Loading,
+  MessageBox,
+  Notification,
+  Message,
+} from 'element-ui';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(Button);
+Vue.use(Row);
+Vue.use(Loading.directive);
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
 
 /* eslint-disable no-new */
 new Vue({
