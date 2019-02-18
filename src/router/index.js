242fde19709cc9380ff7dc878fe0189abd6746c7
Unknown <s1996ch@gmail.com>
2019-02-18 08:19:14 +0330

    Remove hash from url

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Track from '@/views/Track';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     { path: '/', component: Home },
     { path: '/users/:id', component: User },
