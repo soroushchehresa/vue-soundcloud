f9194c5f783415279f46f5730f0e3fa93a0cc350
Soroush Chehresa <s1996ch@gmail.com>
2020-08-17 01:12:11 +0430

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'VUE SOUNDCLOUD',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
